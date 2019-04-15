1aae09ea940aa60f2a4ddb4b304ac25163723cbc
mkozlowski98 <mk394389@students.mimuw.edu.pl>
2019-04-15 11:46:32 +0200

    divide into functions

--- klient.c
+++ klient.c
@@ -1,4 +1,3 @@
-//#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
@@ -7,39 +6,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
+
 #include "structures.h"
 
 #define PORT_NUM "6543"
 
-int main(int argc, char *argv[]) {
-  int sock;
-  char *port_num = PORT_NUM;
+int connect_to_server(char *host, char *port_num) {
+  int sock, err;
   struct addrinfo addr_hints;
   struct addrinfo *addr_result;
-
-  int err;
-  struct RequestId request_id;
-  struct DirList dir_list;
-  struct ClientRequest request;
-  ssize_t len;
-
-  char *list;
-  int file_number;
-  uint32_t begin_address, length;
-  uint16_t name_len;
-
-  if (argc < 2) {
-    printf("Not enough arguments\n");
-    return 1;
-  }
-  else if (argc == 3)
-    port_num = argv[2];
 
   memset(&addr_hints, 0, sizeof(struct addrinfo));
   addr_hints.ai_family = AF_INET;
   addr_hints.ai_socktype = SOCK_STREAM;
   addr_hints.ai_protocol = IPPROTO_TCP;
-  err = getaddrinfo(argv[1], port_num, &addr_hints, &addr_result);
+  err = getaddrinfo(host, port_num, &addr_hints, &addr_result);
 
   if (err != 0)
     printf("error in getaddrinfo\n");
@@ -53,69 +34,103 @@ int main(int argc, char *argv[]) {
 
   freeaddrinfo(addr_result);
 
-  request_id.id = htons(1);
-  len = sizeof(request_id);
+  return sock;
+}
 
-  if (write(sock, &request_id, sizeof(request_id)) != len) {
+void list_request(int sock, uint32_t *dir_len, char **list) {
+  struct RequestId request_id;
+  struct DirList dir_list;
+  ssize_t len;
+
+  request_id.id = htons(1);
+  if (write(sock, &request_id, sizeof(request_id)) != sizeof(request_id)) {
     printf("error in write\n");
   }
 
   len = read(sock, (char *) &dir_list, sizeof(dir_list));
-  if (len < 0)
+  if (len < 0) {
     printf("error in read\n");
-  else {
-    printf("id: %" PRIu16 ", len: %" PRIu32 "\n", ntohs(dir_list.id), ntohl(dir_list.len));
-    list = (char *) malloc(ntohl(dir_list.len) + 1);
-    len = read(sock, list, ntohl(dir_list.len));
-    if (len < 0) {
-      printf("error in read list\n");
-    } else {
-//      printf("%ld\n", len);
-//      printf("%.*s\n", (int) len, list);
-      char *token = (char *) malloc(ntohl(dir_list.len) + 1);
-      strcpy(token, list);
-      token = strtok(token, "|");
-      int i = 1;
-      printf("Lista plików:\n");
-      while (token != NULL) {
-        printf("%d. %s, %ld\n", i++, token, strlen(token));
-        token = strtok(NULL, "|");
-      }
-//      printf("%s\n", list);
-      free(token);
-      printf("Wybierz plik do przesłania:\n");
-      scanf("%d", &file_number);
-      printf("Wybierz adres początku fragmentu:\n");
-      scanf("%d", &begin_address);
-      printf("Wybierz adres końca fragmentu:\n");
-      scanf("%d", &length);
-      length -= begin_address;
-
-      printf("%d %" PRIu32 " %" PRIu32 "\n", file_number, begin_address, length);
-
-      request_id.id = htons(2);
-      len = sizeof(request_id);
-      if (write(sock, &request_id, sizeof(request_id)) != len) {
-        printf("error in write\n");
-      }
-
-      token = (char *) malloc(ntohl(dir_list.len) + 1);
-      strcpy(token, list);
-      token = strtok(token, "|");
-      for (int k = 1; k < file_number; k++)
-        token = strtok(NULL, "|");
-      name_len = strlen(token);
-
-      request.begin_address = htonl(begin_address);
-      request.number_bytes = htonl(length);
-      request.name_len = htons(name_len);
-      len = sizeof(request);
-      if (write(sock, &request, sizeof(request)) != len)
-        printf("error in write\n");
-//      free(token);
-      free(list);
-    }
   }
+
+  *(dir_len) = ntohl(dir_list.len);
+//  printf("%d\n", *(dir_len));
+  *(list) = (char *) realloc(*(list), *(dir_len) + 1);
+  len = read(sock, *(list), *(dir_len));
+  if (len < 0) {
+    printf("error in read\n");
+  }
+//  printf("%.*s\n", (int) len, *(list));
+}
+
+int main(int argc, char *argv[]) {
+  int sock;
+  char *port_num = PORT_NUM;
+
+  struct ClientRequest request;
+  struct RequestId request_id;
+  ssize_t len;
+
+  char *list;
+  uint32_t dir_len = 0;
+  int file_number;
+  uint32_t begin_address, length;
+  uint16_t name_len;
+
+  if (argc < 2) {
+    printf("Not enough arguments\n");
+    return 1;
+  }
+  else if (argc == 3)
+    port_num = argv[2];
+
+  sock = connect_to_server(argv[1], port_num);
+
+  list_request(sock, &dir_len, &list);
+
+//  printf("%.*s, %d\n", 21, list, dir_len);
+
+  char *token = (char *) malloc(dir_len + 1);
+  strcpy(token, list);
+  token = strtok(token, "|");
+  int i = 1;
+  printf("Lista plików:\n");
+  while (token != NULL) {
+    printf("%d. %s, %ld\n", i++, token, strlen(token));
+    token = strtok(NULL, "|");
+  }
+
+  printf("Wybierz plik do przesłania:\n");
+  scanf("%d", &file_number);
+  printf("Wybierz adres początku fragmentu:\n");
+  scanf("%d", &begin_address);
+  printf("Wybierz adres końca fragmentu:\n");
+  scanf("%d", &length);
+  length -= begin_address;
+
+  printf("%d %" PRIu32 " %" PRIu32 "\n", file_number, begin_address, length);
+
+  request_id.id = htons(2);
+  len = sizeof(request_id);
+  if (write(sock, &request_id, sizeof(request_id)) != len) {
+    printf("error in write\n");
+  }
+
+  token = (char *) malloc(dir_len + 1);
+  strcpy(token, list);
+  token = strtok(list, "|");
+  for (int k = 1; k < file_number; k++)
+    token = strtok(NULL, "|");
+  name_len = strlen(token);
+
+  request.begin_address = htonl(begin_address);
+  request.number_bytes = htonl(length);
+  request.name_len = htons(name_len);
+  len = sizeof(request);
+  if (write(sock, &request, sizeof(request)) != len)
+    printf("error in write\n");
+  if (write(sock, token, name_len) != name_len)
+    printf("error in write\n");
+  free(list);
 
   if (close(sock) < 0)
     printf("error in close\n");
--- serwer.c
+++ serwer.c
@@ -11,7 +11,46 @@
 #include "structures.h"
 
 #define PORT_NUM 6543
-#define QUEUE_LENGTH 5
+
+void send_list(int msg_sock, char *dir_name) {
+  struct DirList dir_list;
+  struct dirent *dir;
+  DIR *d;
+  uint32_t dir_len = 0, file_len;
+  char *list;
+  int i = 0;
+
+  d = opendir(dir_name);
+  list = (char *) calloc(1, sizeof(char));
+  if (d != NULL) {
+    while ((dir = readdir(d)) != NULL) {
+      if (dir->d_type != DT_DIR) {
+        file_len = strlen(dir->d_name);
+        dir_len += file_len;
+        if (i == 0) {
+          list = (char *) realloc(list, dir_len + 1);
+          strcpy(list, dir->d_name);
+        } else {
+          dir_len++;
+          list = (char *) realloc(list, dir_len + 3);
+          strcat(list, "|");
+          strcat(list, dir->d_name);
+        }
+        i++;
+      }
+    }
+
+    printf("%s, %ld\n", list, strlen(list));
+    closedir(d);
+    dir_list.id = htons(1);
+    dir_list.len = htonl(dir_len);
+    if (write(msg_sock, &dir_list, sizeof(dir_list)) != sizeof(dir_list))
+      printf("error in write\n");
+    if (write(msg_sock, list, dir_len) != strlen(list))
+      printf("error in write with list\n");
+    free(list);
+  }
+}
 
 int main(int argc, char *argv[]) {
   int sock, msg_sock;
@@ -22,13 +61,11 @@ int main(int argc, char *argv[]) {
 
   struct ClientRequest client_request;
   struct RequestId request_id;
-  struct DirList dir_list;
   ssize_t len;
 
-  struct dirent *dir;
-  DIR *d;
-  uint32_t dir_len, file_len;
-  char *list;
+  uint32_t begin_address, number_bytes;
+  uint16_t name_len;
+  char *file_name;
 
   if (argc == 3)
     port_num = (short) strtol(argv[2], NULL, 0);
@@ -42,7 +79,7 @@ int main(int argc, char *argv[]) {
   if (bind(sock, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
     printf("error in bind\n");
 
-  if (listen(sock, QUEUE_LENGTH) < 0)
+  if (listen(sock, SOMAXCONN) < 0)
     printf("error in listen\n");
 
   for (;;) {
@@ -53,56 +90,37 @@ int main(int argc, char *argv[]) {
       printf("error in accept\n");
 
     printf("client connected\n");
-    len = read(msg_sock, (char *) &request_id, sizeof(request_id));
-    dir_len = 0;
-    if (len < 0)
-      printf("error in read\n");
-    if (ntohs(request_id.id) == 1) {
-      printf("listing dir\n");
-      int i = 0;
-      d = opendir(argv[1]);
-      list = (char *) calloc(1, sizeof(char));
-      if (d != NULL) {
-        while ((dir = readdir(d)) != NULL) {
-          if (dir->d_type != DT_DIR) {
-            file_len = strlen(dir->d_name);
-            dir_len += file_len;
-            if (i == 0) {
-              list = (char *) realloc(list, dir_len + 1);
-              strcpy(list, dir->d_name);
-            }
-            else {
-              dir_len++;
-              list = (char *) realloc(list, dir_len + 3);
-              strcat(list, "|");
-              strcat(list, dir->d_name);
-            }
-            i++;
-          }
-        }
-        printf("%s, %ld\n", list, strlen(list));
 
-        closedir(d);
-        dir_list.id = htons(1);
-        dir_list.len = htonl(dir_len);
-        if (write(msg_sock, &dir_list, sizeof(dir_list)) != sizeof(dir_list))
-          printf("error in write\n");
-        if (write(msg_sock, list, dir_len) != strlen(list))
-          printf("error in write with list\n");
-        len = read(msg_sock, (char *) &request_id, sizeof(request_id));
-        if (len < 0)
-          printf("error in read\n");
-        else {
-          printf("id: %" PRIu16 "\n", ntohs(request_id.id));
+    for (int i = 0; i < 2; i++) {
+      len = read(msg_sock, (char *) &request_id, sizeof(request_id));
+      if (len < 0)
+        printf("error in read\n");
+
+      switch (ntohs(request_id.id)) {
+        case 1:
+          printf("listing dir\n");
+          send_list(msg_sock, argv[1]);
+          break;
+        case 2:
+          printf("send file\n");
           len = read(msg_sock, (char *) &client_request, sizeof(client_request));
-          if (len < 0)
+          if (len < 0) {
             printf("error in read\n");
-          else {
-            printf("begin address: %" PRIu32 ", length: %" PRIu32 ", len_name: %" PRIu16 "\n",
-              ntohl(client_request.begin_address), ntohl(client_request.number_bytes), ntohs(client_request.name_len));
+            break;
           }
-        }
-        free(list);
+          begin_address = ntohl(client_request.begin_address);
+          number_bytes = ntohl(client_request.number_bytes);
+          name_len = ntohs(client_request.name_len);
+          file_name = (char *) malloc(name_len + 1);
+          len = read(msg_sock, file_name, name_len);
+          if (len < 0) {
+            printf("error in read\n");
+            break;
+          }
+          printf("file name is: %.*s\n", (int) len, file_name);
+          free(file_name);
+          i = 2;
+          break;
       }
     }
 //    prev_len = 0;
